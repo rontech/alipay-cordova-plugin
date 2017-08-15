d331d183f1ee441bebf38d48b71723a7b7257c48
DUKAYASHUO <duke.gz.zip@gmail.com>
2017-08-15 10:19:24 +0800

    Add alipay cordova plugin in android.

---
+++ src/android/PayResult.java
@@ -0,0 +1,66 @@
+package alipay;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import android.text.TextUtils;
+
+public class PayResult {
+	private String resultStatus;
+	private String result;
+	private String memo;
+
+	public PayResult(Map<String, String> rawResult) {
+		if (rawResult == null) {
+			return;
+		}
+
+		for (String key : rawResult.keySet()) {
+			if (TextUtils.equals(key, "resultStatus")) {
+				resultStatus = rawResult.get(key);
+			} else if (TextUtils.equals(key, "result")) {
+				result = rawResult.get(key);
+			} else if (TextUtils.equals(key, "memo")) {
+				memo = rawResult.get(key);
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "resultStatus={" + resultStatus + "};memo={" + memo
+				+ "};result={" + result + "}";
+	}
+
+	public JSONObject toJson(){
+		Map<String, String> payResultsMap = new HashMap<String, String>() {{
+			put("resultStatus", resultStatus);
+			put("memo", memo);
+			put("result", result);
+		}};
+		return new JSONObject(payResultsMap);
+	}
+
+	/**
+	 * @return the resultStatus
+	 */
+	public String getResultStatus() {
+		return resultStatus;
+	}
+
+	/**
+	 * @return the memo
+	 */
+	public String getMemo() {
+		return memo;
+	}
+
+	/**
+	 * @return the result
+	 */
+	public String getResult() {
+		return result;
+	}
+}
